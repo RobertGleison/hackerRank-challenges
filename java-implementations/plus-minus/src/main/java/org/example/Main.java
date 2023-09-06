6fa914d30982a94e6f0f2de8857cd062dcb60a9a
Robert <robertgleison12@gmail.com>
2023-09-06 13:07:21 +0100

    Final resolution of plus-minus with try-catch

--- java-implementations/plus-minus/src/main/java/org/example/Main.java
+++ java-implementations/plus-minus/src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
@@ -10,29 +9,37 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
 
-        Locale.setDefault(Locale.US);
-        Scanner sc = new Scanner(System.in);
+        try (Scanner sc = new Scanner(System.in)) {
+            Locale.setDefault(Locale.US);
+            int size = sc.nextInt();
+            sc.nextLine();
+            String line = sc.nextLine();
+            String[] StrNumbers = line.split(" ");
 
-        int size = sc.nextInt();
-        List<Integer> numbers = Stream.of(sc.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
-        plusMinus(numbers);
+            if (StrNumbers.length == size) {
+                List<Integer> numbers = Stream.of(StrNumbers).
+                                        map(Integer::parseInt).
+                                        collect(Collectors.toList());
+                plusMinus(numbers);
+            }
+        } catch (RuntimeException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 
     public static void plusMinus(List<Integer> list) {
-        int positives = 0, negatives = 0, zeros = 0;
-        double negativeRatio = 0.0, positiveRatio = 0.0, zeroRatio = 0.0;
-        for (Integer i : list){
-            if (i<0) negatives++;
-            if (i>0) positives++;
-            if (i==0) zeros++;
+        double positives = 0.0, negatives = 0.0, zeros = 0.0;
+        for (Integer i : list) {
+            if (i < 0.0) negatives++;
+            if (i > 0.0) positives++;
+            if (i == 0.0) zeros++;
         }
-        negativeRatio = negatives / list.size();
-        positiveRatio = positives / list.size();
-        zeroRatio = zeros / list.size();
+        double negativeRatio = negatives / list.size();
+        double positiveRatio = positives / list.size();
+        double zeroRatio = zeros / list.size();
 
-        System.out.println(
-                String.format("%.6f%n",positiveRatio) +
-                String.format("%.6f%n",negativeRatio) +
-                String.format("%.6f%n",zeroRatio)
-        );
-}}
+        System.out.println(String.format("%.6f%n", positiveRatio) +
+                           String.format("%.6f%n", negativeRatio) +
+                           String.format("%.6f%n", zeroRatio));
+    }
+}
