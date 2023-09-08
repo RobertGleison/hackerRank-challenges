2d4e5b4e817c2e13afb415a95328ba5d810b7dbe
Robert <robertgleison12@gmail.com>
2023-09-08 12:16:50 +0100

    Implemented resolution with functional programming

--- mini-maxSum/src/Main.java
+++ mini-maxSum/src/Main.java
@@ -9,7 +9,20 @@ public class Main {
         String input = sc.nextLine();
         String[] strArray = input.trim().split(" ");
         int[] intArray = Arrays.stream(strArray).mapToInt(Integer::parseInt).toArray();
+        int minSum = minSum(intArray);
+        int maxSum = maxSum(intArray);
+        System.out.println(minSum + " " + maxSum);
 
+    }
+
+    public static int maxSum(int[] array){
+        int min = Arrays.stream(array).min().orElse(0);
+        return Arrays.stream(array).filter(n->n!= min).reduce(0,(x,y)->x+y);
+    }
+
+    public static int minSum(int[] array){
+        int max = Arrays.stream(array).max().orElse(0);
+        return Arrays.stream(array).filter(n->n!= max).reduce(0,(x,y)->x+y);
     }
 
 
