eb797d96fec3f63764f9cbc873d9b7be9bfb6cb5
Robert <robertgleison12@gmail.com>
2023-09-09 11:32:47 +0100

    Get the code more readable and change types of variables from int to long

--- mini-maxSum/src/Main.java
+++ mini-maxSum/src/Main.java
@@ -1,29 +1,20 @@
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-
         String input = sc.nextLine();
-        String[] strArray = input.trim().split(" ");
-        int[] intArray = Arrays.stream(strArray).mapToInt(Integer::parseInt).toArray();
-        int minSum = minSum(intArray);
-        int maxSum = maxSum(intArray);
-        System.out.println(minSum + " " + maxSum);
-
+        String[] numbersAsString = input.trim().split(" ");
+        long[] nums = Arrays.stream(numbersAsString).mapToLong(Long::parseLong).toArray();
+        miniMax(nums);
     }
 
-    public static int maxSum(int[] array){
-        int min = Arrays.stream(array).min().orElse(0);
-        return Arrays.stream(array).filter(n->n!= min).reduce(0,(x,y)->x+y);
+    public static void miniMax(long[] array) {
+        long sum = Arrays.stream(array).reduce(0, Long::sum);
+        long min = Arrays.stream(array).min().orElse(0);
+        long max = Arrays.stream(array).max().orElse(0);
+        System.out.println((sum-max) + " " + (sum-min));
     }
-
-    public static int minSum(int[] array){
-        int max = Arrays.stream(array).max().orElse(0);
-        return Arrays.stream(array).filter(n->n!= max).reduce(0,(x,y)->x+y);
-    }
-
-
 }
+
+
