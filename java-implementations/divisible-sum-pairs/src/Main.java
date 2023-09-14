ff38f6ca5c58342247b39a45e692f96967d85709
Robert <robertgleison12@gmail.com>
2023-09-14 09:49:06 +0100

    Implemented Divisible Sum Pairs HackerRank challenge

---
+++ java-implementations/divisible-sum-pairs/src/Main.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int arraySize = sc.nextInt();
+        int k = sc.nextInt();
+        sc.nextLine();
+        String input = sc.nextLine();
+        int[] nums = Arrays.stream(input.trim().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        //hashmap
+        if (arraySize == nums.length) {
+            int result = divisibleSumPairs(nums, k);
+            System.out.println(result);
+        }
+    }
+
+    public static int divisibleSumPairs(int[] nums, int k) {
+        Map<Integer, Integer> remainderFreq = new HashMap<>();
+        int count = 0;
+
+        for (int num : nums) {
+            int remainder = num % k;
+            int complement = (k - remainder) % k;
+
+            if (remainderFreq.containsKey(complement)) {
+                count += remainderFreq.get(complement);
+            }
+
+            remainderFreq.put(remainder, remainderFreq.getOrDefault(remainder, 0) + 1);
+        }
+
+        return count;
+    }
+}
