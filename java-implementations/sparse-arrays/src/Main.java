f03b12b4a005a33ec498167a77f4d436aca7bfae
Robert <robertgleison12@gmail.com>
2023-09-16 11:04:23 +0100

    Implemented final version of Sparse Arrays

--- java-implementations/sparse-arrays/src/Main.java
+++ java-implementations/sparse-arrays/src/Main.java
@@ -7,32 +7,36 @@ public class Main {
         sc.nextLine();
         List<String> names = new ArrayList<>(size);
         List<String> query = new ArrayList<>();
-        for (int i = 0; i < size; i++){
+        for (int i = 0; i < size; i++) {
             names.add(sc.nextLine());
         }
         int querySize = sc.nextInt();
         sc.nextLine();
-        for (int i = 0; i < querySize; i++){
+        for (int i = 0; i < querySize; i++) {
             query.add(sc.nextLine());
         }
-        if((names.size()==size) && (query.size() == querySize))  queryString(names, query);
+        if ((names.size() == size) && (query.size() == querySize))
+            queryString(names, query);
+        }
+
+    //  HashMap implementation
+    public static void queryString(List<String> names, List<String> query) {
+        Map<String, Integer> words = new LinkedHashMap<>();
+        query.forEach(name -> words.putIfAbsent(name, 0));
+        for (String nameToFind : names) {
+            if (words.containsKey(nameToFind)) {
+                int newValue = words.get(nameToFind) + 1;
+                words.put(nameToFind, newValue);
+            }
+        }
+        words.values().forEach(System.out::println);
+
     }
 
-    //HashMap implementation
-    public static void queryString(List<String> names, List<String> query){
-        Map<String,Integer> words = new LinkedHashMap<>();
-        for(String name : query){
-            if (!words.containsKey(name)){
-            words.put(name, 0);
-            }
-        }
-        for(String nameToFind : names){
-            if(words.containsKey(nameToFind)){
-                int newValue = words.get(nameToFind)+1;
-                words.put(nameToFind,newValue);
-            }
-        }
-        words.values().forEach(x-> System.out.println(x));
-
+    public static void matchingStrings(List<String> strings, List<String> queries) {
+        final List<Integer> responseList = new ArrayList<>();
+        queries.forEach(query -> {responseList.add((int) strings.stream().filter(str -> str.equals(query)).count());});
+        responseList.forEach(System.out::println);
     }
 }
+
